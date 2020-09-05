a780439ca2333327e77717eac5e14e35f4fdcb78
yyyyuan <alexyuan357@gmail.com>
2020-09-04 23:45:12 -0700

    Add new set of interfaces into code base.

---
+++ src/Quark/src/Quark.java
@@ -0,0 +1,13 @@
+/*
+ * Copyright Bo Yuan
+ *
+ * Interface created for Quark, which defines basic functionalities of Quark based on quantum theory.
+ */
+
+
+public interface Quark {
+    /*
+     * Define the interaction behavior with quark.
+     */
+    void interactionWithQuark();
+}
